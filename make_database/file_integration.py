d52c84605b64302be90cb15c813dc3b1c77db60c
lavistar <wila@tistory.com>
2019-12-22 14:04:26 +0900

    fix: try to integrate 4 files

--- make_database/file_integration.py
+++ make_database/file_integration.py
@@ -18,11 +18,11 @@ def split_str(row):
     inst_list_ = row.replace("\'", '').replace(' ', '').replace('[', '').replace(']', '').split(',')
     return inst_list_
 
-def get_csv_list(csv_file_list):
+def get_csv_list(csv_file_list, folderpath):
     """csv file list를 받아서 읽어 df로 전환 뒤 리스트에 저장해 return"""
     letters_dictionary = []
     for file_name in csv_file_list:
-        df = pd.read_csv(f'../csv/{file_name}', encoding='UTF-8' )
+        df = pd.read_csv(f'{folderpath}/{file_name}', encoding='UTF-8' )
         letters_dictionary.append(df.iloc[:,1:])
     # print(letters_dictionary[0].duplicated().iloc[10:15])
     return letters_dictionary
@@ -47,23 +47,24 @@ def read_two_lines(p_dict):
     return rearranged_dict
 
 
-folder_path = os.path.join('..', 'csv')
-csv_file_list = file_list(folder_path, name='letters_list')
-df_list = get_csv_list(csv_file_list)
-integrated_df = df_list[0]
-print('len(df_list):',len(df_list))
-for i in range(1,len(df_list)):
-    integrated_df = pd.concat([integrated_df, df_list[i]], axis=0)
+if __name__ == '__main__':
+    folder_path = os.path.join('..', 'csv')
+    csv_file_list = file_list(folder_path, name='letters_list')
+    df_list = get_csv_list(csv_file_list)
+    integrated_df = df_list[0]
+    print('len(df_list):',len(df_list))
+    for i in range(1,len(df_list)):
+        integrated_df = pd.concat([integrated_df, df_list[i]], axis=0)
 
-print(integrated_df)
-integrated_df.to_excel('../excel/hsk_words_list_3100.xlsx', index=None, header=['word'])
-# print(integrated_file)
+    print(integrated_df)
+    integrated_df.to_excel('../excel/hsk_words_list_3100.xlsx', index=None, header=['word'])
+    # print(integrated_file)
 
-# print(csv_file_list)
-
-
+    # print(csv_file_list)
 
 
-# dict_df = pd.DataFrame(rearranged_dict)
-# dict_df.to_csv('../csv/letters_dictionary_combined.csv')
+
+
+    # dict_df = pd.DataFrame(rearranged_dict)
+    # dict_df.to_csv('../csv/letters_dictionary_combined.csv')
 
--- nav_hsk_words/file_integration.py
+++ nav_hsk_words/file_integration.py
@@ -5,13 +5,13 @@ import pandas as pd
 if __name__ == '__main__':
 
     folder_path = os.path.join('..', 'csv2')
-    csv_file_list = fi.file_list(folder_path, name='letters_list')
-    df_list = fi.get_csv_list(csv_file_list)
+    csv_file_list = fi.file_list(folder_path, name='rel_len')
+    df_list = fi.get_csv_list(csv_file_list, folder_path)
+    print(df_list)
     integrated_df = df_list[0]
     print('len(df_list):', len(df_list))
     for i in range(1, len(df_list)):
         integrated_df = pd.concat([integrated_df, df_list[i]], axis=0)
 
-    print(integrated_df)
-    integrated_df.to_excel('../excel/hsk_words_list_3100.xlsx', index=None, header=['word'])
+    print(integrated_df.head())
     # print(integrated_file)
